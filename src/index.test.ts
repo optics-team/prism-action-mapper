abbab8ebecf52973b8a66e76fa3498548175c24a
Warren Seymour <warren@fountainhead.tech>
2017-09-13 13:58:13 +0100

    Initial commit

---
+++ src/index.test.ts
@@ -0,0 +1,195 @@
+import { Server } from 'hapi';
+import { ResourceBackend } from '@optics/prism/security';
+import { ReadItem } from '@optics/prism/action';
+
+import { ActionMapper, ActionMap } from './index';
+
+const registerAction = jest.fn();
+const registerBackend = jest.fn();
+
+const server: Server = {
+  log: jest.fn(),
+  plugins: {
+    'prism': {
+      registerAction
+    },
+
+    'prism-security': {
+      registerBackend
+    }
+  },
+} as any;
+
+const source = 'mockSource' as any;
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+describe('when actionMap is empty', () => {
+  it('does nothing', () => {
+    const mapper = new ActionMapper(server, source, {});
+
+    mapper.register({
+      name: 'test_table'
+    } as any);
+
+    expect(registerBackend).not.toHaveBeenCalled();
+    expect(registerAction).not.toHaveBeenCalled();
+  });
+});
+
+describe('when a `backend` entry is specified', () => {
+  it('registers a resource backend', () => {
+    const mapper = new ActionMapper(server, source, {
+      test_table: {
+        backend: {}
+      }
+    });
+
+    mapper.register({
+      name: 'test_table'
+    } as any);
+
+    expect(registerBackend).toHaveBeenCalledTimes(1);
+    const backend = registerBackend.mock.calls[0][0];
+
+    expect(backend).toBeInstanceOf(ResourceBackend);
+  });
+
+  describe('when options are specified', () => {
+    it('they are passed to ResourceBackend constructor', () => {
+      const mapper = new ActionMapper(server, source, {
+        test_table: {
+          backend: {
+            options: {
+              redact: '**TEST**'
+            }
+          }
+        }
+      });
+
+      mapper.register({
+        name: 'test_table'
+      } as any);
+
+      const backend = registerBackend.mock.calls[0][0];
+      expect(backend._options.redact).toBe('**TEST**');
+    });
+  });
+
+  describe('when merge values are specified', () => {
+    it('they are merged with the ResourceBackend instance', () => {
+      const mapper = new ActionMapper(server, source, {
+        test_table: {
+          backend: {
+            merge: {
+              schema: {
+                $schema: 'TEST'
+              } as any
+            }
+          }
+        }
+      });
+
+      mapper.register({
+        name: 'test_table'
+      } as any);
+
+      const backend = registerBackend.mock.calls[0][0];
+      expect(backend.schema).toEqual({
+        $schema: 'TEST'
+      });
+    });
+  });
+});
+
+describe('when action entries are specified', () => {
+  it('registers an Action', () => {
+    const mapper = new ActionMapper(server, source, {
+      test_table: {
+        readItem: {}
+      }
+    });
+
+    mapper.register({
+      name: 'test_table',
+      primaryKeys: ['id']
+    } as any);
+
+    expect(registerAction).toHaveBeenCalledTimes(1);
+    const readItem = registerAction.mock.calls[0][0];
+
+    expect(readItem).toBeInstanceOf(ReadItem);
+  });
+
+  describe('when options are specified', () => {
+    it('they are passed to the Action constructor', () => {
+      const mapper = new ActionMapper(server, source, {
+        test_table: {
+          readCollection: {
+            options: {
+              pageSize: 5
+            }
+          }
+        }
+      });
+
+      mapper.register({
+        name: 'test_table',
+        primaryKeys: ['id']
+      } as any);
+
+      expect(registerAction).toHaveBeenCalledTimes(1);
+      const readCollection = registerAction.mock.calls[0][0];
+
+      expect(readCollection._options.pageSize).toBe(5);
+    });
+  });
+
+  describe('when merge values are specified', () => {
+    it('they are merged with the Action instance', () => {
+      const mapper = new ActionMapper(server, source, {
+        test_table: {
+          readItem: {
+            merge: {
+              path: '/custom_path'
+            }
+          }
+        }
+      });
+
+      mapper.register({
+        name: 'test_table',
+        primaryKeys: ['id']
+      } as any);
+
+      const readItem = registerAction.mock.calls[0][0];
+      expect(readItem.path).toBe('/custom_path');
+    });
+  });
+
+  describe('when `public` is `true`', () => {
+    it('sets `routeConfig.auth.mode` to `optional`', () => {
+      const mapper = new ActionMapper(server, source, {
+        test_table: {
+          readItem: {
+            public: true
+          }
+        }
+      });
+
+      mapper.register({
+        name: 'test_table',
+        primaryKeys: ['id']
+      } as any);
+
+      const readItem = registerAction.mock.calls[0][0];
+      expect(readItem.routeConfig).toEqual({
+        auth: {
+          mode: 'optional'
+        }
+      });
+    });
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,119 @@
+import { Server } from 'hapi';
+
+import { TableDescription } from 'collimator/lib/inspectors/tables';
+import { ViewDescription } from 'collimator/lib/inspectors/views';
+import { ExtendedTableDescription, ExtendedViewDescription } from 'collimator';
+
+import { Source } from '@optics/prism/source';
+import { Resource } from '@optics/prism/resource';
+
+import { Action, ReadItem, ReadCollection, CreateItem, UpdateItem, DeleteItem } from '@optics/prism/action';
+import { Options as ReadCollectionOptions } from '@optics/prism/action/ReadCollection';
+import {
+  Resource as ResourceBackend,
+  Options as ResourceBackendOptions
+} from '@optics/prism/security/backend/Resource';
+
+type Table = TableDescription & ExtendedTableDescription;
+type View = ViewDescription & ExtendedViewDescription;
+
+export interface ResourceBackendConfiguration {
+  merge: Partial<ResourceBackend>;
+  options: Partial<ResourceBackendOptions>;
+}
+
+export interface ActionConfiguration {
+  public: boolean;
+  merge: Partial<Action>;
+  options: any;
+}
+
+export interface ActionMapEntry {
+  resource: Partial<Resource>;
+
+  readCollection: Partial<ActionConfiguration> & Partial<{
+    options: Partial<ReadCollectionOptions>;
+  }>;
+
+  readItem: Partial<ActionConfiguration>;
+  createItem: Partial<ActionConfiguration>;
+  updateItem: Partial<ActionConfiguration>;
+  deleteItem: Partial<ActionConfiguration>;
+
+  backend: Partial<ResourceBackendConfiguration>;
+}
+
+type ActionMapEntryKey = 'readItem' | 'readCollection' | 'createItem' | 'updateItem' | 'deleteItem';
+
+export interface ActionMap {
+  [tableName: string]: Partial<ActionMapEntry>;
+}
+
+export class ActionMapper {
+  constructor(protected _server: Server, protected _source: Source, protected _actionMap: ActionMap) { }
+
+  register(entity: Table | View) {
+    const mapEntry = this._actionMap[entity.name];
+
+    if (!mapEntry) {
+      return;
+    }
+
+    const resource = {
+      ...entity as Table,
+      ...mapEntry.resource,
+      source: this._source
+    };
+
+    this._registerBackend(resource, mapEntry);
+    this._registerAction(resource, mapEntry, 'readItem', ReadItem);
+    this._registerAction(resource, mapEntry, 'readCollection', ReadCollection);
+    this._registerAction(resource, mapEntry, 'createItem', CreateItem);
+    this._registerAction(resource, mapEntry, 'updateItem', UpdateItem);
+    this._registerAction(resource, mapEntry, 'deleteItem', DeleteItem);
+  }
+
+  protected _registerBackend(resource: Resource, mapEntry: Partial<ActionMapEntry>) {
+    if (!mapEntry.backend) {
+      return;
+    }
+
+    this._server.log('info', `Registering security backend on resource '${resource.name}`);
+
+    const backend = new ResourceBackend(resource, mapEntry.backend.options);
+    Object.assign(backend, mapEntry.backend.merge);
+
+    this._server.plugins['prism-security'].registerBackend(backend);
+  }
+
+  protected _registerAction<K extends ActionMapEntryKey>(
+    resource: Resource,
+    mapEntry: ActionMap[K],
+    key: K,
+    ActionClass: new (...args: any[]) => Action
+  ) {
+    const config = mapEntry[key];
+
+    if (!config) {
+      return;
+    }
+
+    this._server.log('info', `Registering action '${name}' on resource '${resource.name}'`);
+
+    const action = new ActionClass(resource, config.options);
+
+    Object.assign(action, config.merge);
+
+    if (config.public) {
+      Object.assign(action, {
+        routeConfig: {
+          auth: {
+            mode: 'optional'
+          }
+        }
+      });
+    }
+
+    this._server.plugins.prism.registerAction(action);
+  }
+}
